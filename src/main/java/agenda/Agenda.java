78933d0787d14adced52d0dca09e27c987c9e782
Salomé Chevailler <Salom Chevailler@LAPTOP-M0PMU7L5>
2020-12-11 14:22:19 +0100

    Méthodes findByTitle() et isFreeFor() + tests

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -7,15 +7,15 @@ import java.util.*;
  * Description : An agenda that stores events
  */
 public class Agenda {
+
     /**
      * Adds an event to this agenda
      *
      * @param e the event to add
      */
     private List<Event> myEvents = new LinkedList<>();
-    
+
     public void addEvent(Event e) {
-        // TODO : implémenter cette méthode
         this.myEvents.add(e);
     }
 
@@ -26,21 +26,63 @@ public class Agenda {
      * @return and iteraror to the events that occur on that day
      */
     public List<Event> eventsInDay(LocalDate day) throws Exception {
-        // TODO : implémenter cette méthode
-        if(this.myEvents.isEmpty()){
+        if (this.myEvents.isEmpty()) {
             throw new Exception("Pas d'evenement dans cet Agenda");
         }
-        
+
         List<Event> resultat = new LinkedList<>();
-        
-        for ( Event e : myEvents){
-            if (e.isInDay(day)){
+
+        for (Event e : myEvents) {
+            if (e.isInDay(day)) {
                 resultat.add(e);
             }
         }
-        
+
         return resultat;
-        
+
+    }
+
+    /**
+     * Trouver les événements de l'agenda en fonction de leur titre
+     *
+     * @param title le titre à rechercher
+     * @return les événements qui ont le même titre
+     */
+    public List<Event> findByTitle(String title) throws Exception {
+        if (this.myEvents.isEmpty()) {
+            throw new Exception("Pas d'evenement dans cet Agenda");
+        }
+        List<Event> titreIdentique = new LinkedList<>();
+        for (Event e : myEvents) {
+            if (e.getTitle().equals(title)) {
+                titreIdentique.add(e);
+            }
+        }
+        return titreIdentique;
+    }
+
+    /**
+     * Déterminer s’il y a de la place dans l'agenda pour un événement
+     *
+     * @param e L'événement à tester (on se limitera aux événements simples)
+     * @return vrai s’il y a de la place dans l'agenda pour cet événement
+     */
+    public boolean isFreeFor(Event e) throws Exception {
+        if (this.myEvents.isEmpty()) {
+            throw new Exception("Pas d'evenement dans cet Agenda");
+        }
+        boolean r = true;
+
+        for (Event event : myEvents) {
+            if (e.isInDay(event.getStart().toLocalDate())) {
+                if (e.getStart().toLocalTime().isBefore(event.getStart().toLocalTime()) || e.getStart().toLocalTime().equals(event.getStart().toLocalTime())){
+                    if (e.getStart().plus(e.getDuration()).toLocalTime().isAfter(event.getStart().toLocalTime()) || e.getStart().plus(e.getDuration()).toLocalTime().equals(event.getStart().toLocalTime())){
+                        r = false;
+                    }
+                }
+            }
+
+        }
+        return r;
     }
 }
-    
--- src/test/java/agenda/AgendaTest.java
+++ src/test/java/agenda/AgendaTest.java
@@ -20,6 +20,15 @@ public class AgendaTest {
 
     // November 1st, 2020, 22:30
     LocalDateTime nov_1__2020_22_30 = LocalDateTime.of(2020, 11, 1, 22, 30);
+    
+    // November 1st, 2020, 21:00
+    LocalDateTime nov_1_2020_21_00 = LocalDateTime.of(2020, 11, 1, 21, 00);
+    
+    // November 1st, 2020, 23:00
+    LocalDateTime nov_1_2020_12_00 = LocalDateTime.of(2020, 11, 1, 12, 00);
+    
+    // November 2nd, 2020, 11:00
+    LocalDateTime nov_2_2020_11_00 = LocalDateTime.of(2020, 11, 2, 11, 00);
 
     // 120 minutes
     Duration min_120 = Duration.ofMinutes(120);
@@ -27,8 +36,11 @@ public class AgendaTest {
     // A simple event
     // November 1st, 2020, 22:30, 120 minutes
     Event simple = new Event("Simple event", nov_1__2020_22_30, min_120);
-
-    // A Weekly Repetitive event ending at a given date
+    Event simple2 = new Event("Simple event", nov_2_2020_11_00, min_120);
+    Event simple3 = new Event("Simple event", nov_1_2020_21_00, min_120);
+    Event simple4 = new Event("Simple event", nov_1_2020_12_00, min_120);
+    
+// A Weekly Repetitive event ending at a given date
     RepetitiveEvent fixedTermination = new FixedTerminationEvent("Fixed termination weekly", nov_1__2020_22_30, min_120, ChronoUnit.WEEKS, jan_5_2021);
 
     // A Weekly Repetitive event ending after a give number of occurrrences
@@ -53,5 +65,18 @@ public class AgendaTest {
         assertTrue(agenda.eventsInDay(nov_1_2020).contains(neverEnding));
     }
 
+    @Test
+    public void testFindByTitle() throws Exception {
+        assertEquals(1, agenda.findByTitle("Simple event").size());
+        agenda.addEvent(simple2);
+        assertEquals(2, agenda.findByTitle("Simple event").size());
+    }
+    
+    @Test 
+    public void testIsFreeFor() throws Exception {
+        assertTrue(agenda.isFreeFor(simple2));
+        assertFalse(agenda.isFreeFor(simple3));
+        assertTrue(agenda.isFreeFor(simple4));
+    }
 
 }
